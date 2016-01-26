eaa6de1c392e79b3ed8c39f9b9330c00c8ef5e70
YoonJungBoo <coma333ryu@gmail.com>
2016-01-25 21:54:26 -0500

    random test code added

--- nBaseball.go
+++ nBaseball.go
@@ -3,8 +3,49 @@
  */
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"fmt"
+	"flag"
+	_"time"
+)
+
+var arrComputerNumbers []int
+var iNumberMaxLength = flag.Int("maxL", 3, "")
+
+func makeNumbers(iLength int) int{
+	randomNumber := rand.New(rand.NewSource(10))
+
+	if(iLength != 0) {
+		arrComputerNumbers = append(arrComputerNumbers, randomNumber.Intn(10))
+		return makeNumbers(iLength-1)
+	}else{
+		return 0
+	}
+
+
+}
 
 func main() {
-	fmt.Println("Hello")
+	flag.Parse()
+
+	flag.Args()
+
+	//fmt.Println(flag.Args())
+	//fmt.Println(*iNumberMaxLength)
+
+	/*
+	rand.Seed(int64(time.Now().Nanosecond()))
+	for i:=0; i< 5; i++{
+		fmt.Println(rand.Intn(10))
+	}
+	*/
+
+	for i:=0; i< 5; i++{
+		fmt.Println(rand.Intn(10))
+	}
+
+	makeNumbers(*iNumberMaxLength)
+
+	fmt.Println(arrComputerNumbers)
 }
