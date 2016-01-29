f6b64d1537257571e2a77cf87f452bef122ba756
YoonJungBoo <coma333ryu@gmail.com>
2016-01-28 21:47:13 -0500

    code updated

--- nBaseball.go
+++ nBaseball.go
@@ -11,27 +11,54 @@ import (
 )
 
 var arrComputerNumbers []int
-var iNumberMaxLength = flag.Int("maxL", 3, "")
+var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
+var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. Only first number never use 0 \n 2. All number never use 0 \n 3. 0 to 9 numbers use")
 
 /*
 	Option
-		1. First number never use 0
+		1. Only first number never use 0
 		2. All number never use 0
 		3. 0 to 9 numbers use
  */
-func makeNumbers(iLength int) bool{
+func makeNumbers(iLength int, strOption string) bool{
 	rand.Seed(int64(time.Now().Nanosecond()))
+	randomNumber := rand.Intn(10)
 
 	if(iLength != 0) {
-		randomNumber := rand.Intn(10)
-		//First number never use 0 (It is option)
-		if(len(arrComputerNumbers) == 0){
-			for(randomNumber == 0){
+		if(strOption == "1"){
+			if(len(arrComputerNumbers) == 0){
+				for(randomNumber == 0){
+					randomNumber = rand.Intn(10)
+				}
+			}else{
+				for v := range arrComputerNumbers {
+					if(arrComputerNumbers[v] == randomNumber){
+						randomNumber = rand.Intn(10)
+					}
+				}
+			}
+		}else if(strOption == "2"){
+
+		}else{
+
+		}
+
+		arrComputerNumbers = append(arrComputerNumbers, randomNumber)
+		return makeNumbers(iLength-1,strOption)
+	}else{
+		for i:=0; i< len(arrComputerNumbers)-1 ; i++ {
+			if(arrComputerNumbers[*iNumberMaxLength-1] == arrComputerNumbers[i]){
+				fmt.Println("There is duplication of number")
+				fmt.Println("arrComputerNumbers[*iNumberMaxLength-1]",arrComputerNumbers[*iNumberMaxLength-1])
+				fmt.Println("arrComputerNumbers[i]",arrComputerNumbers[i])
 				randomNumber = rand.Intn(10)
+				fmt.Println("randomNumber",randomNumber)
+				if(randomNumber == arrComputerNumbers[*iNumberMaxLength-1]){
+					randomNumber = rand.Intn(10)
+				}
+				arrComputerNumbers[i] = randomNumber
 			}
 		}
-		arrComputerNumbers = append(arrComputerNumbers, randomNumber)
-		return makeNumbers(iLength-1)
 	}
 
 	return true
@@ -43,11 +70,12 @@ func main() {
 
 	flag.Args()
 
-	/*
-	if (makeNumbers(*iNumberMaxLength)){
+	fmt.Println(flag.Args())
+
+	if (makeNumbers(*iNumberMaxLength, *strCreateNumberOption)){
 
 	}
-	*/
+
 
 	fmt.Println(arrComputerNumbers)
 }
