cc7e81b83ef171de470859e58e9f7873793b8a40
YoonJungBoo <coma333ryu@gmail.com>
2016-02-02 21:25:13 -0500

    source refactoring

--- nBaseball.go
+++ nBaseball.go
@@ -4,97 +4,50 @@
 package main
 
 import (
-	"math/rand"
 	"fmt"
 	"flag"
-	"time"
 	"log"
 	"os"
+	_"bufio"
+	"nBaseball/util"
+	"nBaseball/numbers"
 )
 
 var arrComputerNumbers []int
 var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
-var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. Only first number never use 0 \n 2. All number never use 0 \n 3. 0 to 9 numbers use")
+var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. First number does no use 0 only. \n 2. All number do not use 0. \n 3. 0 to 9 numbers use")
 
-/*
-	Function	: makeNumber
-	Arguments	:
-		iLength		= length of making numbers
-		strOption
-			1. Only first number never use 0
-			2. All number never use 0
-			3. 0 to 9 numbers use
-	Desc	: to make random number
- */
-func makeNumbers(iLength int, strOption string) bool{
-	rand.Seed(int64(time.Now().Nanosecond()))
-	randomNumber := rand.Intn(10)
-	bResult := false
-	if(iLength != 0) {
-		if(strOption == "1"){
-			if(len(arrComputerNumbers) == 0){
-				for(randomNumber == 0){
-					randomNumber = rand.Intn(10)
-				}
-			}
-		}else if(strOption == "2"){
 
-		}else{
 
-		}
 
-		arrComputerNumbers = append(arrComputerNumbers, randomNumber)
-		return makeNumbers(iLength-1,strOption)
-	}
-
-	if (compareNumbers()){
-		bResult = true
-	}else{
-		bResult = false
-	}
-
-	return bResult
-}
-
-/*
-	Function	: compareNumbers
-	Arguments	: nothing
-	Desc		: to comapare all number of arrComputerNumbers and to change duplication number
- */
-func compareNumbers() bool{
-	bCheck := true
-	rand.Seed(int64(time.Now().Nanosecond()))
-	randomNumber := rand.Intn(10)
-	for i:=0; i <len(arrComputerNumbers); i++ {
-		for j:=i+1; j <len(arrComputerNumbers); j++{
-			fmt.Println(arrComputerNumbers[i])
-			fmt.Println(arrComputerNumbers[j])
-			if(arrComputerNumbers[i] == arrComputerNumbers[j]){
-				for(arrComputerNumbers[i] == randomNumber || (i == 0 && randomNumber == 0)){
-					randomNumber = rand.Intn(10)
-				}
-
-				arrComputerNumbers[i] = randomNumber
-				bCheck = false
-			}
-		}
-	}
-
-	//fmt.Println("arrComputerNumbers",arrComputerNumbers)
-
-	if(!bCheck){
-		compareNumbers()
-	}
-	return true
-}
 func main() {
 	flag.Parse()
 
 	flag.Args()
 
 	//fmt.Println(flag.Args())
+	//fmt.Println("*iNumberMaxLength",*iNumberMaxLength)
 
-	if (makeNumbers(*iNumberMaxLength, *strCreateNumberOption)){
+
+	if(!util.CheckArgNumberLength(*iNumberMaxLength)){
+		log.Fatal("Making number length is 3 to 5.")
+		os.Exit(-1)
+	}
+
+	//scanner := bufio.NewScanner(os.Stdin)
+	//for scanner.Scan() {
+	//	line := scanner.Text()
+	//	if line == "exit" {
+	//		os.Exit(0)
+	//	}
+	//	fmt.Println(line) // Println will add back the final '\n'
+	//}
+	//if err := scanner.Err(); err != nil {
+	//	fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	//}
+
+
+	if (numbers.MakeRandomNumbersbyComputer(*iNumberMaxLength, *strCreateNumberOption, arrComputerNumbers)){
 
 	}else{
 		log.Fatal("Making number is fail.")
