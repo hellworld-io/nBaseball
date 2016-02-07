f1b45244302557dfec7bc69194dc51f69c654f60
YoonJungBoo <coma333ryu@gmail.com>
2016-02-06 19:36:59 -0500

    comment updated

--- nBaseball.go
+++ nBaseball.go
@@ -17,9 +17,6 @@ var arrComputerNumbers []int
 var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
 var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. First number does no use 0 only. \n 2. All number do not use 0. \n 3. 0 to 9 numbers use")
 
-
-
-
 func main() {
 	flag.Parse()
 
@@ -34,21 +31,19 @@ func main() {
 		os.Exit(-1)
 	}
 
-	//scanner := bufio.NewScanner(os.Stdin)
-	//for scanner.Scan() {
-	//	line := scanner.Text()
-	//	if line == "exit" {
-	//		os.Exit(0)
-	//	}
-	//	fmt.Println(line) // Println will add back the final '\n'
-	//}
-	//if err := scanner.Err(); err != nil {
-	//	fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	//}
-
 	arrComputerNumbers = numbers.MakeRandomNumbersbyComputer(*iNumberMaxLength, *strCreateNumberOption)
 	if (arrComputerNumbers != nil){
-
+		//scanner := bufio.NewScanner(os.Stdin)
+		//for scanner.Scan() {
+		//	line := scanner.Text()
+		//	if line == "exit" {
+		//		os.Exit(0)
+		//	}
+		//	fmt.Println(line) // Println will add back the final '\n'
+		//}
+		//if err := scanner.Err(); err != nil {
+		//	fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		//}
 	}else{
 		log.Fatal("Making number is fail.")
 		os.Exit(-1)
