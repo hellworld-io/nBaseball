ade68197432b9e71e49831210f2c7faf4db73783
YoonJungBoo <coma333ryu@gmail.com>
2016-02-08 21:24:05 -0500

    user number checking sources updated

--- nBaseball.go
+++ nBaseball.go
@@ -12,11 +12,13 @@ import (
 	"nBaseball/util"
 	"nBaseball/numbers"
 	"time"
+	"strconv"
 )
 
 var arrComputerNumbers []int
 var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
 var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. First number does no use 0 only. \n 2. All number do not use 0. \n 3. 0 to 9 numbers use")
+var strResult string
 
 func main() {
 	flag.Parse()
@@ -32,22 +34,34 @@ func main() {
 		os.Exit(-1)
 	}
 
-	arrComputerNumbers = numbers.MakeRandomNumbersbyComputer(*iNumberMaxLength, *strCreateNumberOption)
+	arrComputerNumbers = numbers.MakeRandomNumbersByComputer(*iNumberMaxLength, *strCreateNumberOption)
 
 	if (arrComputerNumbers != nil){
 		startSecond := time.Now()
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Computer is ready. \nIf you want to exit, you will input exit. \nWhen you start, I will recode time. \nAre you ready? (Y/N)")
+		fmt.Print("Computer is ready. \nIf you want to exit, you will input exit. \nWhen you start, It will be recoded time.\n")
 		for scanner.Scan() {
 			line := scanner.Text()
 
-			if line == "N" {
-				fmt.Print("Ok! See you.")
+			if line == "exit" {
 				os.Exit(0)
 			}
 
-			if line == "exit" {
-				os.Exit(0)
+			iLine, err := strconv.Atoi(line);
+			if err != nil {
+				fmt.Printf("%q does not looks like a number.\n", line)
+			}else{
+				if (!util.CheckUserNumberLength(line, *iNumberMaxLength)){
+					fmt.Printf("Max number length is %d\n",*iNumberMaxLength)
+				}else{
+					strResult = numbers.CheckUserNumber(iLine, arrComputerNumbers)
+
+					if(strResult == "Out"){
+
+					}else{
+						fmt.Printf("%q result is %q\n", iLine, strResult)
+					}
+				}
 			}
 
 			if line == "check"{
@@ -60,9 +74,8 @@ func main() {
 				fmt.Printf("Time Diff == %d:%d \n",totalMin,totalSec)
 
 			}
-
-			//fmt.Println(line) // Println will add back the final '\n'
 		}
+
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		}
