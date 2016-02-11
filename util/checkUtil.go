5589ab340bb9f3282ea47e5f8ee7fbd685bdd3f7
YoonJungBoo <coma333ryu@gmail.com>
2016-02-10 21:22:04 -0500

    functions name changed CheckUserInputNumber function added

--- util/checkUtil.go
+++ util/checkUtil.go
@@ -12,10 +12,34 @@ func CheckArgNumberLength(iNumberLength int) bool{
 	return true
 }
 
-func CheckUserNumberLength(strUserNumberLength string, iMaxNumberLength int) bool{
-	if len(strUserNumberLength) != iMaxNumberLength {
+/*
+	1. Function		: CheckUserNumberLength
+	2. Arguments	:
+		strUserNumberLength	= user input numbers
+		iMaxNumberLength	= random number max length
+	3. Desc			: to compare max number length with user input numbers
+ */
+func CheckUserNumberLength(strUserInputNumber string, iMaxNumberLength int) bool{
+	if len(strUserInputNumber) != iMaxNumberLength {
 		return false
 	}
 
+	return true
+}
+
+/*
+	1. Function		: CheckUserInputNumber
+	2. Arguments	:
+		strUsrInputNumbers	= user input numbers
+	3. Desc			: to check duplication numbers for inputted user numbers
+ */
+func CheckUserInputNumber(strUsrInputNumbers string) bool{
+	for idx := 0; idx < len(strUsrInputNumbers); idx++{
+		for jdx := idx+1; jdx < len(strUsrInputNumbers); jdx++{
+			if (strUsrInputNumbers[idx] == strUsrInputNumbers[jdx]){
+				return false
+			}
+		}
+	}
 	return true
 }
