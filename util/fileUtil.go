2f4dbeb780a6b48dc9d4d98b298f56bd39f68297
YoonJungBoo <coma333ryu@gmail.com>
2016-02-23 22:09:59 -0500

    fileUtil.go updating

--- util/fileUtil.go
+++ util/fileUtil.go
@@ -9,15 +9,27 @@ import (
 
 var JsonData []map[string]interface{}
 
-func ReadJsonFile(strFileName string) {
+type UserResult struct {
+	UserResults []struct{
+		No string `json:"no"`
+		Name string `json:"name"`
+		Time string `json:"time"`
+		Try string `json:"try"`
+	} `json:"result"`
+}
+
+
+func (u *UserResult) ReadJsonFile(strFileName string) {
 	file, err := ioutil.ReadFile(strFileName)
 
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(-1)
-	}//
+	}
 
-	err = json.Unmarshal(file, &JsonData)
+	userResultJsonData := &UserResult{}
+
+	err = json.Unmarshal(file, &userResultJsonData)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(-1)
