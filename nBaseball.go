6289679262ed6bdb8c711306ba11c01d2c053d25
YoonJeongbu <coma333ryu@gmail.com>
2017-04-11 17:43:37 +0900

    changing all sources for refactoring

--- nBaseball.go
+++ nBaseball.go
@@ -5,22 +5,15 @@ package main
 
 import (
 	"fmt"
-	"flag"
-	"log"
-	"os"
-	"nBaseball/util"
-	"nBaseball/numbers"
 	"time"
-	"strconv"
-	"bufio"
-	_"encoding/json"
+	"nBaseball/flags"
 )
 
-var arrComputerNumbers = []int{1,2,3}	//Test data
-var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
-var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. All number do not use 0. \n 2. First number does no use 0 only. \n 3. 0 to 9 numbers use")
-var strResult string
-var bClear = bool(false)
+//var arrComputerNumbers = []int{1,2,3}	//Test data
+//var iNumberMaxLength = flag.Int("maxL", 3, "must create 3 to 5 number length")
+//var strCreateNumberOption = flag.String("lOpt", "1", "Option \n 1. All number do not use 0. \n 2. First number does no use 0 only. \n 3. 0 to 9 numbers use")
+//var strResult string
+//var bClear = bool(false)
 var iTryTotalCount int
 
 
@@ -56,112 +49,118 @@ func setResult() (result []string){
 }
 
 func main() {
-	flag.Parse()
 
-	flag.Args()
+	max, opt := flags.SetFlags()
 
-	//fmt.Println(flag.Args())
-	//fmt.Println("*iNumberMaxLength",*iNumberMaxLength)
+	fmt.Println("max =====> ", max)
+	fmt.Println("opt =====> ", opt)
 
-
-	if !util.CheckArgNumberLength(*iNumberMaxLength) {
-		log.Fatal("Making number length is 3 to 5.")
-		os.Exit(-1)
-	}
-
-	//[TODO] for test temporary comment
-	//numbers.MakeRandomNumbersByComputer(*iNumberMaxLength, *strCreateNumberOption)
-	//arrComputerNumbers
-	//numbers.ArrRandomNumbers
-
-	if  arrComputerNumbers != nil {
-		startSecond := time.Now()
-
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Computer is ready. \nIf you want to exit, you will input exit. \nWhen you started, It will be recoded time.\n")
-
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			if line == "exit" {
-				os.Exit(0)
-			}
-
-			if !bClear {
-				iTryTotalCount++
-				if _, err := strconv.Atoi(line); err != nil {
-					fmt.Printf("%q does not looks like a number.\n", line)
-				}else{
-					if  !util.CheckUserNumberLength(line, *iNumberMaxLength) {
-						fmt.Printf("Max number length is %d\n",*iNumberMaxLength)
-					}else{
-						if util.CheckUserInputNumber(line) {
-							strResult = numbers.CompareUserNumbersAndRandomNumbers(line, arrComputerNumbers)
-
-							if strResult == "Out" {
-								endSecond := time.Now()
-
-								showResults(startSecond, endSecond)
-								//os.Exit(-1)
-
-								bClear = true
-							}else{
-								fmt.Printf("%q result is %q\n", line, strResult)
-							}
-						}else{
-							fmt.Println("Do not use duplication number")
-						}
-					}
-				}
-			}else{
-				//fmt.Println(setResult())
-
-				for _, val := range setResult() {
-					fmt.Fprintln(os.Stdout, val)
-				}
-
-				//var strFileName = string( "./data/UserResult.json")
-				//util.ReadJsonFile(strFileName)
-				//var uinfo = &util.UserResult{}
-				//fmt.Println(util.UserResult{})
-				//fmt.Println(uinfo)
-				/*
-				fmt.Println("Here is currently results.")
-				var strFileName = string( "./data/UserResult.json")
-				util.ReadJsonFile(strFileName)
-				fmt.Println(util.JsonData[0]["Name"].(string))
-				util.JsonData[0]["Name"] = "aaa"
-				fmt.Println(util.JsonData[0]["Name"].(string))
-				fmt.Println(util.JsonData)
-				jsonUserResult, err := json.Marshal(util.JsonData)
-
-				if err != nil {
-					log.Fatal("Making json file error!!! ")
-					os.Exit(-1)
-				}
-
-
-				jsonUpdate, err := os.Create("./data/UserResult.json")
-
-				if err != nil {
-					log.Fatal("Making json file error!!! ")
-					os.Exit(-1)
-				}
-				//defer jsonUpdate.Close()
-
-				jsonUpdate.Write(jsonUserResult)
-				jsonUpdate.Close()
-				*/
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-	}else{
-		log.Fatal("Making number is fail.")
-		os.Exit(-1)
-	}
+	//flag.Parse()
+	//
+	//flag.Args()
+	//
+	////fmt.Println(flag.Args())
+	////fmt.Println("*iNumberMaxLength",*iNumberMaxLength)
+	//
+	//
+	//if !util.CheckArgNumberLength(*iNumberMaxLength) {
+	//	log.Fatal("Making number length is 3 to 5.")
+	//	os.Exit(-1)
+	//}
+	//
+	////[TODO] for test temporary comment
+	////numbers.MakeRandomNumbersByComputer(*iNumberMaxLength, *strCreateNumberOption)
+	////arrComputerNumbers
+	////numbers.ArrRandomNumbers
+	//
+	//if  arrComputerNumbers != nil {
+	//	startSecond := time.Now()
+	//
+	//	scanner := bufio.NewScanner(os.Stdin)
+	//	fmt.Print("Computer is ready. \nIf you want to exit, you will input exit. \nWhen you started, It will be recoded time.\n")
+	//
+	//	for scanner.Scan() {
+	//		line := scanner.Text()
+	//
+	//		if line == "exit" {
+	//			os.Exit(0)
+	//		}
+	//
+	//		if !bClear {
+	//			iTryTotalCount++
+	//			if _, err := strconv.Atoi(line); err != nil {
+	//				fmt.Printf("%q does not looks like a number.\n", line)
+	//			}else{
+	//				if  !util.CheckUserNumberLength(line, *iNumberMaxLength) {
+	//					fmt.Printf("Max number length is %d\n",*iNumberMaxLength)
+	//				}else{
+	//					if util.CheckUserInputNumber(line) {
+	//						strResult = numbers.CompareUserNumbersAndRandomNumbers(line, arrComputerNumbers)
+	//
+	//						if strResult == "Out" {
+	//							endSecond := time.Now()
+	//
+	//							showResults(startSecond, endSecond)
+	//							//os.Exit(-1)
+	//
+	//							bClear = true
+	//						}else{
+	//							fmt.Printf("%q result is %q\n", line, strResult)
+	//						}
+	//					}else{
+	//						fmt.Println("Do not use duplication number")
+	//					}
+	//				}
+	//			}
+	//		}else{
+	//			//fmt.Println(setResult())
+	//
+	//			for _, val := range setResult() {
+	//				fmt.Fprintln(os.Stdout, val)
+	//			}
+	//
+	//			//var strFileName = string( "./data/UserResult.json")
+	//			//util.ReadJsonFile(strFileName)
+	//			//var uinfo = &util.UserResult{}
+	//			//fmt.Println(util.UserResult{})
+	//			//fmt.Println(uinfo)
+	//			/*
+	//			fmt.Println("Here is currently results.")
+	//			var strFileName = string( "./data/UserResult.json")
+	//			util.ReadJsonFile(strFileName)
+	//			fmt.Println(util.JsonData[0]["Name"].(string))
+	//			util.JsonData[0]["Name"] = "aaa"
+	//			fmt.Println(util.JsonData[0]["Name"].(string))
+	//			fmt.Println(util.JsonData)
+	//			jsonUserResult, err := json.Marshal(util.JsonData)
+	//
+	//			if err != nil {
+	//				log.Fatal("Making json file error!!! ")
+	//				os.Exit(-1)
+	//			}
+	//
+	//
+	//			jsonUpdate, err := os.Create("./data/UserResult.json")
+	//
+	//			if err != nil {
+	//				log.Fatal("Making json file error!!! ")
+	//				os.Exit(-1)
+	//			}
+	//			//defer jsonUpdate.Close()
+	//
+	//			jsonUpdate.Write(jsonUserResult)
+	//			jsonUpdate.Close()
+	//			*/
+	//		}
+	//	}
+	//
+	//	if err := scanner.Err(); err != nil {
+	//		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	//	}
+	//}else{
+	//	log.Fatal("Making number is fail.")
+	//	os.Exit(-1)
+	//}
 
 	//fmt.Println(arrComputerNumbers)
 }
