77c7ddea32b2771b3ce70a6fdebe02be88ea5417
YoonJeongbu <coma333ryu@gmail.com>
2017-04-12 14:36:44 +0900

    delete comment

--- util/checkUtil.go
+++ util/checkUtil.go
@@ -1,18 +1,6 @@
 package util
 
 /*
-	1. Function		: CheckArgNumberLength
-	2. Arguments	: iNumberLength	= to make max length of number
-	3. Desc			: to check max length
- */
-func CheckArgNumberLength(iNumberLength int) bool{
-	if iNumberLength < 3 || iNumberLength > 5 {
-		return false
-	}
-	return true
-}
-
-/*
 	1. Function		: CheckUserNumberLength
 	2. Arguments	:
 		strUserNumberLength	= user input numbers
